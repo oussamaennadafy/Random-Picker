376442945be07d30e719d8931c9f4f899f49d8d7
oussama <oussama.ennadafy@gmail.com>
2022-09-21 15:27:44 +0100

    complete generating and add some feutures

--- public/main.js
+++ public/main.js
@@ -1,16 +1,19 @@
 'use strict'
 
-const input_of_name = document.getElementById('input_of_name');
 const add_member = document.getElementById('add_member');
 const pick_one_btn = document.getElementById('pick_one');
 const picked_member = document.getElementById('picked_member')
 const container_of_members = document.getElementById('container_of_names');
-const remove_member = document.querySelectorAll('.member');
-const arrOfMembers = [];
 
+//function to add the member
 const addMember = function() {
+ const input_of_name = document.getElementById('input_of_name');
  //check if input is empty
  if(input_of_name.value.trim() !== '') {
+  //check if there is members
+  if(container_of_members.children.length + 1 > 0) {
+   document.getElementById('There_is_no_Member').classList.add('hidden');
+  }
   //create elements
   //////////////////////////
   const div = document.createElement('div');
@@ -28,33 +31,47 @@ const addMember = function() {
   div.appendChild(span);
   div.appendChild(img);
   container_of_members.appendChild(div);
-  //push members to arrays
-  arrOfMembers.push(input_of_name.value);
   //empty the input
   input_of_name.value = '';
+  //remove elements onclick on them
+  const membersCloseIcon = document.querySelectorAll('.member img');
+  membersCloseIcon.forEach(function(ele) {
+   ele.addEventListener('click',function() {
+    //remove the element from the dom
+    this.parentElement.remove();
+    //check if there is no members
+    if(container_of_members.children.length === 0) {
+     document.getElementById('There_is_no_Member').classList.remove('hidden');
+    }
+   })
+  })
  }
 }
 
+//add member to the list with click button
 add_member.addEventListener('click', function() {
  addMember();
 })
 
+//add member with enter in keyboard
 input_of_name.addEventListener("keypress", function (event) {
  if (event.key === "Enter") {
   addMember();
  }
 })
 
+//pick random member
 pick_one_btn.addEventListener('click',function() {
- if(arrOfMembers.length > 1) {
-  const random_index = Math.floor(Math.random() * arrOfMembers.length);
-  picked_member.textContent = arrOfMembers[random_index];
- } 
-});
-
-console.log(remove_member);
-remove_member.forEach(function(el) {
- el.addEventListener('click',function() {
-  console.log(el);
- })
-})
+ if(container_of_members.children.length > 0) {
+  //generate ramdom index 
+  const random_index = Math.floor(Math.random() * container_of_members.children.length);
+  //show the random member
+  picked_member.textContent = container_of_members.children[random_index].textContent;
+  //remove the random member from the list
+  container_of_members.children[random_index].remove();
+ }
+ //check if there is no members
+ if(container_of_members.children.length === 0) {
+  document.getElementById('There_is_no_Member').classList.remove('hidden');
+ }
+ });
